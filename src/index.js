9c161e57fbd2a56e8cd78e4b920a77286a1611af
Katie <katiefaybehrmann@gmail.com>
2022-11-19 11:39:13 -0700

    commit tree 2b50de6b15e5e27346e12fa6be5488f5e06a7fb6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+    const input = document.querySelector('input#searchByID');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (input.value< 1 || input.value >3){
+            title.textContent = "Select a move from the list"
+            summary.textContent = "PLEASE!"
+        }
+
+        else if (1 <= input.value <= 3) {
+            fetch(`http://localhost:3000/movies/${input.value}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+
+                    title.innerText = data.title;
+                    summary.innerText = data.summary;
+                });
+        }
+        
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
